431a8b8929a9c6fd59e1b242305e8dbaa8477f49
Alessandro Casale <webmastercasale@gmail.com>
2024-08-31 10:00:56 +0200

    Aggiornamento del sabato

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -6,11 +6,13 @@ import { UserProfile } from '../../interface/app.interface';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';
 import { alert } from '../../widgets/ui-dialogs';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { PopUpService } from '../../services/popup.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, FormsModule,AnagraficaWrapperComponent],
+  imports: [CommonModule, FormsModule,AnagraficaWrapperComponent,DynamicFormComponent,],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
@@ -20,10 +22,13 @@ export class UsersComponent {
 users$: Observable<UserProfile[]> | undefined;
 
 selectedUser: UserProfile | undefined;
+
+subtitle: string = `Elenco utenti registrati in app`;
 users!: UserProfile[];  
 
 private userService=inject(UserService)
-subtitle: string = `Elenco utenti registrati in app`;
+propertiesModal= inject ( PopUpService ); 
+
 
 ngOnInit(){
   // Carica gli utenti dalla collezione 'users' di Firestore
@@ -39,8 +44,29 @@ deleteUser(arg0: any) {
 }
  
 
-editUser(_t11: any) {
-  throw new Error('Method not implemented.');
+editUser(user: UserProfile) {
+  this.selectedUser = user;
+  
+  let guid = Math.random().toString().replace("0.", "");
+    let InstanceData = {
+      service:'profileForm',
+      editData:this.selectedUser
+    }
+    
+    this.propertiesModal.setNewPopUp(guid, 'DynamicFormComponent', null, 800, null, InstanceData, false, true, "Modifica Utente",'',false)
+    
+
+    this.propertiesModal.outputComponent.subscribe(resulOutputComponent=>{
+      if(resulOutputComponent.guid == guid && resulOutputComponent.name == 'submitForm'){
+        
+       
+      }
+
+      if(resulOutputComponent.guid == guid && resulOutputComponent.name == 'cancelForm'){
+        this.selectedUser = undefined
+        this.propertiesModal.destroyCurrentOpenPopUpByGuid(guid);
+      }
+    })
 }
 
 createNewuser() {
@@ -62,6 +88,9 @@ disableUser(uid: string) {
 
   this.userService.disabledUsersFirebase(uid)
   throw new Error('Method not implemented.');
+}
+updateUser(evt:any){
+
 }
 
 }
