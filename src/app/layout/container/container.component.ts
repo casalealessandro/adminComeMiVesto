ba82e74113c240bc7e25340604ac8e736bf1a9ec
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-05 10:15:52 +0200

    refactor: remove generated and legacy admin baggage

--- src/app/layout/container/container.component.ts
+++ src/app/layout/container/container.component.ts
@@ -1,18 +1,11 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, effect, HostListener, signal, WritableSignal } from '@angular/core';
-import { AnagraficaWrapperComponent } from '../anagrafica-wrapper/anagrafica-wrapper.component';
-import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, effect } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MenuService } from '../../services/menu.service';
 import { HeaderComponent } from '../header/header.component';
 import { MenuComponent } from '../menu/menu.component';
-import { ToolbarComponent } from '../toolbar/toolbar.component';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { PopupWrapperComponent } from '../../components/modal-popup/modal-popup-wrapper/modal-popup-wrapper.component';
-import { OverlayComponent } from '../../components/overlay-component/overlay.component';
-import { AuthService } from '../../services/auth.service';
-
-import {  BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import { MenuService } from '../../services/menu.service';
-
 
 @Component({
   selector: 'app-container',
@@ -25,26 +18,22 @@ import { MenuService } from '../../services/menu.service';
   ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Opzionale, se stai usando Web Components
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ContainerComponent {
   isClose: boolean = false;
   isLogin: boolean = false;
   idTipoUtente: number = -1;
 
-// Modalità del menu: 'side' (a lato), 'over' (sovrapposto), 'push' (spingendo il contenuto)
   mode: 'side' | 'over' | 'push' = 'side';
-   // Observable che rappresenta lo stato del menu (collegato al servizio)
   getIsMenuOpenObservable = this.menuService.getIsMenuOpenObservable;
-
-
-  // Signal per tracciare se il menu è aperto o chiuso
   isMenuOpen: boolean = false;
 
-
-
-  constructor(private auth: AuthService, private menuService: MenuService, private breakpointObserver: BreakpointObserver) {
-
+  constructor(
+    private auth: AuthService,
+    private menuService: MenuService,
+    private breakpointObserver: BreakpointObserver
+  ) {
     effect(() => {
       this.isLogin = !!this.auth.currentUser();
       this.isMenuOpen = this.menuService.isOpenMenu();
@@ -54,47 +43,39 @@ export class ContainerComponent {
   ngOnInit() {
     void this.auth.waitForUser().then(user => this.isLogin = !!user);
     this.updateMenuVisibility(window.innerWidth);
-    //osserva i breakpoint (dimensioni dello schermo) per cambiare la modalità del menu
-    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
-    .subscribe(result => {
-      console.log('result',result)
-      if(result.breakpoints[Breakpoints.XSmall] || result.breakpoints[Breakpoints.Small] ) {
-        this.mode = 'over'; //modalita overlay per schermi piccoli
+
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large
+    ]).subscribe(result => {
+      if (result.breakpoints[Breakpoints.XSmall] || result.breakpoints[Breakpoints.Small]) {
+        this.mode = 'over';
         this.menuService.closeMenu();
-      }else if ( result.breakpoints[Breakpoints.Medium]) {
+      } else if (result.breakpoints[Breakpoints.Medium]) {
         this.mode = 'side';
         this.menuService.openMenu();
-      }
-      else {
-        this.mode ='push'; //modalita overlay per schermi grandi 
+      } else {
+        this.mode = 'push';
         this.menuService.openMenu();
       }
     });
   }
 
-
-
-  /*  // Listener per l'evento resize, per aggiornare la visibilità del menu
-   @HostListener('window:resize', ['$event'])
-   onResize(event: Event) {
-     const windowWidth = (event.target as Window).innerWidth;
-     this.updateMenuVisibility(windowWidth);
-   } */
-
-  // Funzione per aggiornare lo stato del menu in base alla risoluzione dello schermo
   updateMenuVisibility(windowWidth: number) {
     if (windowWidth > 1024) {
       this.menuService.openMenu();
     } else {
       this.menuService.closeMenu();
     }
-
-    console.log('' + this.isMenuOpen)
   }
 
-  // Metodo per passare la funzione toggle al componente Header
   toggleMenu(evt: any) {
     this.menuService.toggleMenu();
   }
-  closeMenu(): void { this.menuService.closeMenu(); }
+
+  closeMenu(): void {
+    this.menuService.closeMenu();
+  }
 }
