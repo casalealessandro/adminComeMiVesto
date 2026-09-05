828b60168ebc8614f3ad86194c98d53c90a5882e
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-05 17:17:06 +0200

    refactor(legacy): remove product feed importer from popup registry

--- src/app/services/entryComponents.ts
+++ src/app/services/entryComponents.ts
@@ -1,13 +1,11 @@
 import { DynamicFormComponent } from "../components/dynamic-form/dynamic-form.component";
 import { ElementComponent } from "../views/form-builder/element/element.component";
 import { OutfitProductsComponent } from "../views/outfit-products/outfit-products.component";
-import { ProductFromFeedComponent } from "../views/outfit-products/product-from-feed/product-from-feed.component";
 
 export const entryComponents= [
     
     { name: "ElementComponent", component: ElementComponent },
     { name: "DynamicFormComponent", component: DynamicFormComponent },
-    { name: "ProductFromFeedComponent", component: ProductFromFeedComponent },
     { name: "OutfitProductsComponent", component: OutfitProductsComponent },
 
    
