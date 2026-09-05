7cc4b5b351076c123bc0765a64abc9a210f58ef6
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-05 10:36:36 +0200

    chore: remove remaining tracked debug artifacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './layout/container/container.component';
-import { OverlayComponent } from "./components/overlay-component/overlay.component";
-import { PopupWrapperComponent } from "./components/modal-popup/modal-popup-wrapper/modal-popup-wrapper.component";
+import { OverlayComponent } from './components/overlay-component/overlay.component';
+import { PopupWrapperComponent } from './components/modal-popup/modal-popup-wrapper/modal-popup-wrapper.component';
 
 @Component({
   selector: 'app-root',
