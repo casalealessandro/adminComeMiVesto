adbeb63c8b84b4b191ef427383b968e7a0e97817
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-08 16:24:37 +0200

    refactor(header): add ComeMiVesto branding config E.3.1

---
+++ src/app/app-header-config.ts
@@ -0,0 +1,7 @@
+import { HeaderConfig } from './services/header-config';
+
+export const comeMiVestoHeaderConfig: HeaderConfig = {
+  logoUrl: 'assets/images/logo.jpg',
+  logoAlt: 'Logo',
+  defaultAvatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg'
+};
