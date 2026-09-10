837f6404b427083ac1447f329968cf268c00eeef
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-10 22:17:23 +0200

    test(affiliate): cover feed creation mapping contract

---
+++ src/app/views/affiliate-catalog/services/affiliate-feed-creation-mapping.service.spec.ts
@@ -0,0 +1,69 @@
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { environment } from '../../../../environments/environment';
+import { AffiliateFeedCreateInput } from '../models/affiliate-catalog-api.models';
+import { AffiliateCatalogService } from './affiliate-catalog.service';
+
+describe('Affiliate feed creation mapping contract', () => {
+  let service: AffiliateCatalogService;
+  let http: HttpTestingController;
+  const baseUrl = `${environment.apiBaseUrl}/admin/affiliate`;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+    service = TestBed.inject(AffiliateCatalogService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => http.verify());
+
+  it('keeps source values returned by the existing feed creation endpoint', () => {
+    const input: AffiliateFeedCreateInput = {
+      networkFeedId: '44191',
+      affiliateProgramId: 'program-1',
+      name: 'Pull&Bear',
+      enabled: true,
+      adapterType: 'PULL_BEAR',
+      readMode: 'WHOLE_FEED',
+      locale: 'it-IT',
+      market: 'IT',
+      rules: '',
+      rulesMapper: '',
+    };
+    let response: any;
+
+    service.createFeed(input).subscribe((value) => response = value);
+
+    const request = http.expectOne(`${baseUrl}/feeds`);
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual(input);
+    request.flush({
+      data: { id: 'feed-1', ...input },
+      sourceValues: {
+        recordsRead: 2,
+        recordsNormalized: 2,
+        categories: ['Jeans', 'T-Shirts'],
+        colors: ['Black'],
+        genders: ['female'],
+      },
+      sourceValuesError: null,
+    });
+
+    expect(response.sourceValues.categories).toEqual(['Jeans', 'T-Shirts']);
+    expect(response.data.networkFeedId).toBe('44191');
+  });
+
+  it('loads ComeMiVesto colors from the existing taxonomy endpoint', () => {
+    let response: any;
+    service.getOutfitColors().subscribe((value) => response = value);
+
+    const request = http.expectOne(`${environment.apiBaseUrl}/gen/outfitColors`);
+    expect(request.request.method).toBe('GET');
+    request.flush([{ id: 'N', value: 'Nero', parent: null, hex: '#000000' }]);
+
+    expect(response).toEqual([{ id: 'N', value: 'Nero', parent: null, hex: '#000000' }]);
+  });
+});
